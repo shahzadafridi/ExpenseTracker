0bfab6f8cd744bbeb0b577889218a94f8be0f3e0
v-shahzadahmad <v-shahzad.ahmad@cxapp.com>
2025-07-09 17:12:42 +0500

    Sync dashboard summary card with transaction data

--- lib/features/home/components /home_header_view.dart
+++ lib/features/home/components /home_header_view.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:income_expense_tracker/features/home/components%20/header_summary_view.dart';
+import 'package:income_expense_tracker/model/TransactionModel.dart';
 
 import '../../../resources/assets_manager.dart';
 import 'header_top_view.dart';
 
 class HomeHeaderView extends StatelessWidget {
-  const HomeHeaderView({super.key});
+  final List<TransactionModel> transactions;
+
+  const HomeHeaderView(this.transactions, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             children: [
-              Image(
+              const Image(
                 image: AssetImage(ImageAssets.cureHomeHeaderBg),
                 width: double.infinity,
                 height: 250.0,
@@ -24,14 +27,14 @@ class HomeHeaderView extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
+                  const Padding(
                     padding: EdgeInsets.only(left: 16, right: 16, top: 40),
                     child: HeaderTopView(),
                   ),
                   Padding(
-                    padding: EdgeInsets.only(left: 12, right: 12, top: 40),
-                    child: HeaderSummaryView(),
-                  )
+                    padding: const EdgeInsets.only(left: 12, right: 12, top: 40),
+                    child: HeaderSummaryView(transactions),
+                  ),
                 ],
               ),
             ],
